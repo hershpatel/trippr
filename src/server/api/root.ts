323b271b21fc0f1bd7fa824930f5fa8ae8f8650f
Hersh 🍫 <phersh05@gmail.com>
2025-01-24 21:00:23 -0500

    fix trpc issue

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,11 +1,18 @@
-import { createCallerFactory, createTRPCRouter } from "~/server/api/trpc";
+import { createCallerFactory, createTRPCRouter, publicProcedure } from "~/server/api/trpc";
+import { z } from "zod";
 
 /**
  * This is the primary router for your server.
  *
  * All routers added in /api/routers should be manually added here.
  */
-export const appRouter = createTRPCRouter({});
+export const appRouter = createTRPCRouter({
+  hello: publicProcedure
+    .input(z.object({ name: z.string().optional() }))
+    .query(({ input }) => {
+      return `Hello ${input.name ?? "world"}!`;
+    }),
+});
 
 // export type definition of API
 export type AppRouter = typeof appRouter;
